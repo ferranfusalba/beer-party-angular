d4666b41c0aa85efacbecac2ffaca73cc44db09e
ferranfusalba <ferran@protonmail.ch>
2022-02-02 18:10:07 +0100

    local fonts, style improv (1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BeerViewComponent } from './beer-view/beer-view.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TagComponent } from './tag/tag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeerViewComponent,
-    BeerListComponent
+    BeerListComponent,
+    TagComponent
   ],
   imports: [
     BrowserModule,
